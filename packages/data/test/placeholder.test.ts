528a169070df13d3275e1bf1757748969a497605
Peter Hale <peter.hale@salesforce.com>
2021-03-11 05:51:56 -0700

    chore: manual revert

--- packages/data/test/placeholder.test.ts
+++ packages/data/test/placeholder.test.ts
@@ -4,5 +4,3 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-
-/* force a write */
