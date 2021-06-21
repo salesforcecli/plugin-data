7126036f6f8d0fa8d038905a3024730642b5ec56
mshanemc <shane.mclaughlin@salesforce.com>
2021-06-21 16:16:01 -0500

    fix: because of the at sign

--- packages/plugin-data/test/commands/batcher.test.ts
+++ packages/plugin-data/test/commands/batcher.test.ts
@@ -20,7 +20,7 @@ let logSpy: sinon.SinonStub;
 let batcher: Batcher;
 
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-// this comment forces a test again.......xy
+// this comment forces a test again.......xy@@
 
 describe('batcher', () => {
   beforeEach(async () => {
