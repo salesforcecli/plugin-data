f5bd404cfa8d3f4a7a60d77a1be491dc43306f88
Peter Hale <peter.hale@salesforce.com>
2022-06-02 15:03:59 -0600

    chore: really fix the test

--- test/commands/force/data/soql/query/dataSoqlQuery.nut.ts
+++ test/commands/force/data/soql/query/dataSoqlQuery.nut.ts
@@ -161,8 +161,7 @@ describe('data:soql:query command', () => {
       expect(queryResult).to.match(
         /ID\s+?NAME\s+?PHONE\s+?WEBSITE\s+?NUMBEROFEMPLOYEES\s+?INDUSTRY\s+?CONTACTS.LASTNAME\s+?CONTACTS.TITLE\s+?CONTACTS.EMAIL/g
       );
-      expect(queryResult).to.match(/\s{50,}?Smith/g);
-      expect(queryResult).to.match(/\s{50,}?Washington/g);
+      expect(queryResult).to.match(/\s{30,30}Smith/g);
       expect(queryResult).to.match(/Total number of records retrieved: 2\./g);
     });
     it('should handle count()', () => {
