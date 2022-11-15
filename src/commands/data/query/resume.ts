8a13b50a90edcf68987880e81bfdf725d7cd80b9
mshanemc <shane.mclaughlin@salesforce.com>
2022-11-15 11:59:50 -0600

    style: loadMessages for examples

--- src/commands/data/query/resume.ts
+++ src/commands/data/query/resume.ts
@@ -4,7 +4,6 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import * as os from 'os';
 import { Messages } from '@salesforce/core';
 import { QueryJobV2 } from 'jsforce/lib/api/bulk';
 import { Flags, SfCommand } from '@salesforce/sf-plugins-core';
@@ -20,7 +19,7 @@ Messages.loadMessages('@salesforce/plugin-data', 'soql.query');
 export class BulkQueryReport extends SfCommand<unknown> {
   public static readonly summary = reportMessages.getMessage('summary');
   public static readonly description = reportMessages.getMessage('description');
-  public static readonly examples = reportMessages.getMessage('examples').split(os.EOL);
+  public static readonly examples = reportMessages.getMessages('examples');
   public static readonly aliases = ['force:data:soql:bulk:report'];
 
   public static flags = {
