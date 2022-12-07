928f388f7f6569655d84d03dc3954f7e741ef3c6
mshanemc <shane.mclaughlin@salesforce.com>
2022-12-07 16:28:38 -0600

    refactor: use array flag from sf-plugins-core

--- src/flags.ts
+++ src/flags.ts
@@ -4,7 +4,6 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { Flags as oclifFlags } from '@oclif/core';
 import { Messages } from '@salesforce/core';
 import {
   Flags,
@@ -12,10 +11,6 @@ import {
   orgApiVersionFlagWithDeprecations,
   requiredOrgFlagWithDeprecations,
 } from '@salesforce/sf-plugins-core';
-
-export const stringArrayFlag = oclifFlags.custom<string[]>({
-  parse: async (input) => Promise.resolve(input.split(',').map((s) => s.trim())),
-});
 
 Messages.importMessagesDirectory(__dirname);
 const messages = Messages.load('@salesforce/plugin-data', 'messages', [
