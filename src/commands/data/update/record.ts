4b72cca9baa41c6a450bb206527ccf68bcf5c398
mshanemc <shane.mclaughlin@salesforce.com>
2023-11-06 09:21:12 -0600

    style: linter updates

--- src/commands/data/update/record.ts
+++ src/commands/data/update/record.ts
@@ -100,7 +100,7 @@ export default class Update extends SfCommand<SaveResult> {
   }
 }
 
-const isSaveResult = (error: SaveError | Error | unknown): error is SaveError => {
+const isSaveResult = (error: unknown): error is SaveError => {
   const se = error as SaveError;
   return Boolean(se.fields && se.errorCode && se.message);
 };
