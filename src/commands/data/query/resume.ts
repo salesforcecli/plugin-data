97e323ab2b47e6f8f57f2b5f5dc2f0e9f958cb0d
mshanemc <shane.mclaughlin@salesforce.com>
2024-02-27 11:30:40 -0600

    fix: don't send 0 polling timeout

--- src/commands/data/query/resume.ts
+++ src/commands/data/query/resume.ts
@@ -5,9 +5,9 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-
 import { Messages } from '@salesforce/core';
-import { QueryJobV2 } from 'jsforce/lib/api/bulk.js';
+import { QueryJobV2 } from 'jsforce/lib/api/bulk2.js';
+import { Record as jsforceRecord } from 'jsforce';
 import {
   Flags,
   loglevel,
@@ -15,15 +15,14 @@ import {
   orgApiVersionFlagWithDeprecations,
   SfCommand,
 } from '@salesforce/sf-plugins-core';
+import { ResumeOptions } from '../../../types.js';
 import { resultFormatFlag } from '../../../flags.js';
 import { displayResults, transformBulkResults } from '../../../queryUtils.js';
-import { FormatTypes } from '../../../reporters.js';
 import { BulkQueryRequestCache } from '../../../bulkDataRequestCache.js';
 
-Messages.importMessagesDirectoryFromMetaUrl(import.meta.url)
+Messages.importMessagesDirectoryFromMetaUrl(import.meta.url);
 const reportMessages = Messages.loadMessages('@salesforce/plugin-data', 'bulk.report');
 // needed by the flags loaded from the other command
-const queryMessages = Messages.loadMessages('@salesforce/plugin-data', 'soql.query');
 
 export class BulkQueryReport extends SfCommand<unknown> {
   public static readonly summary = reportMessages.getMessage('summary');
@@ -33,7 +32,7 @@ export class BulkQueryReport extends SfCommand<unknown> {
   public static readonly deprecateAliases = true;
 
   public static readonly flags = {
-    'target-org': { ...optionalOrgFlagWithDeprecations, summary: queryMessages.getMessage('flags.targetOrg.summary') },
+    'target-org': optionalOrgFlagWithDeprecations,
     'api-version': orgApiVersionFlagWithDeprecations,
     loglevel,
     'result-format': resultFormatFlag,
@@ -43,30 +42,33 @@ export class BulkQueryReport extends SfCommand<unknown> {
       startsWith: '750',
       summary: reportMessages.getMessage('flags.bulkQueryId.summary'),
       aliases: ['bulkqueryid'],
+      exactlyOne: ['bulk-query-id', 'use-most-recent'],
       deprecateAliases: true,
     }),
     'use-most-recent': Flags.boolean({
       summary: reportMessages.getMessage('flags.useMostRecent.summary'),
-      exclusive: ['bulk-query-id'],
+      exactlyOne: ['bulk-query-id', 'use-most-recent'],
     }),
   };
 
   public async run(): Promise<unknown> {
-    const { flags } = await this.parse(BulkQueryReport);
-    const cache = await BulkQueryRequestCache.create();
+    const [{ flags }, cache] = await Promise.all([this.parse(BulkQueryReport), BulkQueryRequestCache.create()]);
     const resumeOptions = await cache.resolveResumeOptionsFromCache(
       flags['bulk-query-id'],
       flags['use-most-recent'],
       flags['target-org'],
       flags['api-version']
     );
-    const job = new QueryJobV2({ ...resumeOptions.options, operation: 'query' });
-    job.jobInfo = resumeOptions.jobInfo;
-    const results = await job.getResults();
-    const queryResult = transformBulkResults(results, '');
+    const job = new QueryJobV2(resumeOptions.options.connection, {
+      id: resumeOptions.jobInfo.id,
+      pollingOptions: getNonZeroTimeoutPollingOptions(resumeOptions.options.pollingOptions),
+    });
+    await job.poll();
+    const results = await job.result();
+    const queryResult = transformBulkResults((await results.toArray()) as jsforceRecord[], resumeOptions.options.query);
 
     if (!this.jsonEnabled()) {
-      displayResults({ ...queryResult }, flags['result-format'] as FormatTypes);
+      displayResults({ ...queryResult }, flags['result-format']);
     }
 
     if (queryResult.result.done) {
@@ -76,3 +78,14 @@ export class BulkQueryReport extends SfCommand<unknown> {
     return queryResult.result;
   }
 }
+
+/**
+ * polling options are retrieved from the cache.
+ * If the data:query used `--async` or `--wait` 0, we'd be passing that to the jsforce poll method,
+ * which means it would never check the actual result, and always throw a timeout error */
+const getNonZeroTimeoutPollingOptions = (
+  pollingOptions: ResumeOptions['options']['pollingOptions']
+): ResumeOptions['options']['pollingOptions'] => ({
+  ...pollingOptions,
+  pollTimeout: Math.max(pollingOptions.pollTimeout, 1000),
+});
