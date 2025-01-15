f95149cb1bdcfd59073b6f292a9efc8d20434ff7
Cristian Dominguez <cdominguez@salesforce.com>
2025-01-15 14:54:05 -0300

    Revert "test: strip LF ending on windows"
    This reverts commit 12f4007f3140003a91ad646ade8e2eb1eee1ab81.

--- src/bulkUtils.ts
+++ src/bulkUtils.ts
@@ -173,7 +173,7 @@ export async function exportRecords(
               Readable.from(
                 res.body.slice(
                   res.body.indexOf(EOL) + 1,
-                  platform() === 'win32' ? res.body.lastIndexOf(lineEndingsMap['LF']) : undefined
+                  platform() === 'win32' ? res.body.lastIndexOf(lineEndingsMap[outputInfo.lineEnding]) : undefined
                 )
               ),
               fs.createWriteStream(outputInfo.filePath, {
@@ -184,7 +184,10 @@ export async function exportRecords(
             ]
           : [
               Readable.from(
-                res.body.slice(0, platform() === 'win32' ? res.body.lastIndexOf(lineEndingsMap['LF']) : undefined)
+                res.body.slice(
+                  0,
+                  platform() === 'win32' ? res.body.lastIndexOf(lineEndingsMap[outputInfo.lineEnding]) : undefined
+                )
               ),
               fs.createWriteStream(outputInfo.filePath),
             ]
