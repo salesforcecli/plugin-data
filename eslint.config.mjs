8975b85823dc4b1978ec17fb3af9bc639a8466ad
Jamie Feingold <jfeingold35@gmail.com>
2026-08-20 16:08:33 -0500

    fix: reworking eslint config (W-23928423) (#1508)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,6 @@
-export { default } from 'eslint-config-salesforce-typescript';
+import tslint from 'eslint-config-salesforce-typescript';
+import plugin from 'eslint-plugin-sf-plugin';
+
+const configs = [...tslint, ...plugin.configs.recommended];
+
+export default configs;
